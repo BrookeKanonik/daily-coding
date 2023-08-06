633ff39ecaf0337df9d22ad34442e6faf094831b
Brooke Kanonik <brookekanonik@gmail.com>
2023-08-06 11:42:42 -0400

    adding day 17 (first level 6 finished)

---
+++ js/day17.js
@@ -0,0 +1,42 @@
+//Instructions:
+
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+
+//Initial code:
+
+function solution(string) {
+
+    let matching = []
+    let capital = /[A-Z]/g
+    for (let i=0; i < string.length; i++){
+      if (string[i].match(capital)){
+        matching.push(` ${string[i]}`)
+      } else {
+        matching.push(string[i])
+      }
+     
+    }
+  return matching.join('')
+}
+
+//Best practices and other solutions:
+
+function solution(string) {
+    return(string.replace(/([A-Z])/g, ' $1'));
+  
+  }
+
+function solution(string) {
+    string = string.split('').map(function (el) {
+      if (el === el.toUpperCase()) {
+        el = ' ' + el
+      }
+      return el
+    })
+    return string.join('')
+  }
