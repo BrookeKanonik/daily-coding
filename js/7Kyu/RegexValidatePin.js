92ade288a502b9acec53871b0d8a0656527766db
Brooke Kanonik <brookekanonik@gmail.com>
2023-08-24 21:54:52 +0000

    adding comments

--- js/7Kyu/RegexValidatePin.js
+++ js/7Kyu/RegexValidatePin.js
@@ -33,13 +33,21 @@ function validatePIN(pin) {
     return /^(\d{4}|\d{6})$/.test(pin)
 }
 
+//Example:
+
+// /^abc$/ 
+// dabcd would not match since abc needs to be at the beginning and it will need to end with c
+// digit = \d and {} is how many you want to see 
+// ^ means the beginning of the string that you are parsing 
+// $ means look at the end
+
 //Other solves to study:
 
 function validatePIN (pin) {
   
     var pinlen = pin.length;
     var isCorrectLength = (pinlen == 4 || pinlen == 6);
-    var hasOnlyNumbers = pin.match(/^\d+$/);
+    var hasOnlyNumbers = pin.match(/^\d+$/); // + means at least one 
       
     if(isCorrectLength && hasOnlyNumbers){
       return true;
