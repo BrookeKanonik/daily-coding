706beddb0fea885d798d57584fbcbd9c7ba7dbab
Brooke Kanonik <brookekanonik@gmail.com>
2023-08-31 10:07:11 -0400

    adding 8kyu challenge

---
+++ js/8Kyu/SquaringAnArgument.js
@@ -0,0 +1,16 @@
+//Instructions:
+
+/*
+Now you have to write a function that takes an argument and returns the square of it.
+* @param {number} number - the number we would like to square 
+* @return {number} number squared - the number we squared 
+*/
+
+//Initial code (works and is best practice):
+
+function square (num){ //our parameter will always need to be a number (may be negative) 
+return num * num //can also write as an arrow function but both are proper 
+}
+
+//result: we will need to return the number squared
+//example: 5 -> 25 
