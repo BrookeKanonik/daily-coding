efa8076ebc9cda1b12036099109d207eb548b0ff
Brooke Kanonik <brookekanonik@gmail.com>
2023-09-05 19:13:18 -0400

    corrected instructions

--- js/6Kyu/PersistentBugger.js
+++ js/6Kyu/PersistentBugger.js
@@ -1,28 +1,12 @@
 //Instructions:
 /*
-function persistence(num) {
-    let number = String(num)
-    let newNum 
-    let count = 0
-  if (number.length === 1){
-    return 0
-  } else {
-      while (number.length !== 1){
-     newNum =  number.split('').map((element, i) => {
-     if (i!== number.length-1){
-       return element + '*'
-     } else {
-       return element 
-     }
-   }).join('')
-        console.log(number)
-   number = String(eval(newNum))   
-        count ++ 
-  }
-    return count
-    
-  }
-}
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+For example (Input --> Output):
+
+39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+4 --> 0 (because 4 is already a one-digit number)
 */
 
 //Initial code (works):
