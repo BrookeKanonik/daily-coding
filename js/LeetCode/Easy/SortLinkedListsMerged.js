9dcf0bb07c730579371f9f32265c92a6b0b0c7ae
Brooke Kanonik <brookekanonik@gmail.com>
2023-11-08 17:37:42 -0500

    adding 8kyu challenge and leetcode problem

---
+++ js/LeetCode/Easy/SortLinkedListsMerged.js
@@ -0,0 +1,64 @@
+//Instructions:
+
+/*
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+ 
+
+Example 1:
+
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
+*/
+
+//Initial code (works somewhat but is saying you cannot iterate for list1):
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+    //check if both are empty
+    if (list1.length === 0 && list2.length === 0){
+      return list1
+    } else {
+      let together = [...list1,...list2]
+      console.log(together)
+      return together.sort((a,b)=> a-b)
+    }
+    //check if one is empty after merging
+    //sort in order 
+   };
+   
+   
+
+//Best practice:
+
