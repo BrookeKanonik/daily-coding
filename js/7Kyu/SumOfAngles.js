9f193fbd969e25c341c480ddf73d438036cc0923
Brooke Kanonik <brookekanonik@gmail.com>
2023-12-06 15:05:29 -0500

    adding 7kyu challenge

---
+++ js/7Kyu/SumOfAngles.js
@@ -0,0 +1,15 @@
+//Instructions:
+
+/*
+Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+*/
+
+//Initial code (works):
+
+function angle(n) {
+    return (n-2) * 180 
+  }
+
+//Arrow function example:
+
+const angle = n => (n - 2) * 180;
