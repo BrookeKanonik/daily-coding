55bb9d7a45094a6a71174d104d8a7a1915884733
Brooke Kanonik <brookekanonik@gmail.com>
2024-01-06 16:34:22 -0500

    modifying original code and including best practice

--- js/5Kyu/MaximumSubarraySum.js
+++ js/5Kyu/MaximumSubarraySum.js
@@ -12,26 +12,43 @@ Empty list is considered to have zero greatest sum. Note that the empty list or 
 
 //Initial thoughts:
 
-var maxSequence = function(arr){
-
-    let total1= arr[0]
-    let total2 = arr[0]
-    if (Math.max(...arr) < 0 || arr.length === 0){
-      return 0
-    }else {
-        for (let i = 1; i <arr.length; i++){
-          console.log(total1,total2)
-          if (total2 + arr[i] > total2){
-            total2 += arr[i]
-           // total1 += arr[i]
-          } else {
-            if (total1 < total2){
-              total1 = total2
-            }
-            total2 = 0
-          }
+var maxSequence = function (arr) {
+  let total1 = arr[0];
+  let total2 = arr[0];
+  let runningSum = arr[0];
+  if (Math.max(...arr) < 0 || arr.length === 0) {
+    return 0;
+  } else {
+    for (let i = 1; i < arr.length; i++) {
+      console.log(total1, total2);
+      if (total2 + arr[i] > total2) {
+        total2 += arr[i];
+        runningSum += arr[i];
+        // total1 += arr[i]
+      } else {
+        if (total1 < total2 && runningSum < total1) {
+          total1 = total2;
+        } else {
+          total1 = runningSum;
         }
-      return total1 > total2 ? total1 : total2
+        total2 = 0;
+        if (runningSum < total1) {
+          runningSum = total1;
+        }
+      }
     }
-  
+    return total1 > total2 ? total1 : total2;
   }
+};
+
+//Best practice:
+
+var maxSequence = function(arr){
+    var min = 0, ans = 0, i, sum = 0;
+    for (i = 0; i < arr.length; ++i) {
+      sum += arr[i];
+      min = Math.min(sum, min);
+      ans = Math.max(ans, sum - min);
+    }
+    return ans;
+}
