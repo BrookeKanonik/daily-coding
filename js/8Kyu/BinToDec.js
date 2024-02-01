7ad48bacc517327302da351a82fbe2db24ac0fbe
Brooke Kanonik <brookekanonik@gmail.com>
2024-02-01 15:33:06 -0500

    adding 8kyu challenge

---
+++ js/8Kyu/BinToDec.js
@@ -0,0 +1,11 @@
+//Instructions:
+
+/*
+Complete the function which converts a binary number (given as a string) to a decimal number.
+*/
+
+//Initial code (works and best practice):
+
+function binToDec(bin) {
+    return parseInt(bin,2)
+  }
