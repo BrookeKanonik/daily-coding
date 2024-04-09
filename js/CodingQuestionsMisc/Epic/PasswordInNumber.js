6476c1c6d0fe20516960262ba2a92c944dcc56d1
Brooke Kanonik <brookekanonik@gmail.com>
2024-04-08 22:07:39 -0400

    finished permutation problem

---
+++ js/CodingQuestionsMisc/Epic/PasswordInNumber.js
@@ -0,0 +1,6 @@
+//Instructions:
+
+/*
+Given a password in number : Write an algorithm to print all possible combinations of that password. Hint: Try to go from all possible combinations of lower bound to the valid upper bounds.
+*/
+
--- js/CodingQuestionsMisc/Epic/PermutationWords.js
+++ js/CodingQuestionsMisc/Epic/PermutationWords.js
@@ -8,30 +8,39 @@ Get a string(word) from user, then make every possible permutation words. Ex)int
 
 //need to start from the first letter and then rearrange the following ones. ensure no duplicates.
 
-function permuteWords(word){
-    let storedWords = []
-    for (let i=0; i<word.length; i++){
-        //store the words somehow 
-        let lettersSoFar = []
-    }
-}
+// function permuteWords(word){
+//     let storedWords = []
+//     for (let i=0; i<word.length; i++){
+//         //store the words somehow 
+//         let lettersSoFar = []
+//     }
+// }
 
 //Solution (and reference):
+function biggerFunc(nums){
+    const arrayOfArraysOfPermutations = permute(nums);
+    for (let k = 0; k <arrayOfArraysOfPermutations.length; k++){
+        arrayOfArraysOfPermutations[k] = arrayOfArraysOfPermutations[k].join('')
+    }
+    return [...new Set (arrayOfArraysOfPermutations)]; //will return an array with values if wanting to make a string add .join(' ')
+}
 
 function permute(nums) {
     let result = [];
     if (nums.length === 0) return [];
     if (nums.length === 1) return [nums];
     for (let i = 0; i < nums.length; i++) {
-      const currentNum = nums[i];
-      const remainingNums = nums.slice(0, i).concat(nums.slice(i +  1));
-      const remainingNumsPermuted = permute(remainingNums);
-     for (let j = 0; j < remainingNumsPermuted.length; j++) {
-        const permutedArray = [currentNum].concat(remainingNumsPermuted[j]);
-        result.push(permutedArray);
-      }
+        const currentNum = nums[i];
+        const remainingNums = nums.slice(0, i).concat(nums.slice(i +  1));
+        const remainingNumsPermuted = permute(remainingNums);
+        for (let j = 0; j < remainingNumsPermuted.length; j++) {
+            const permutedArray = [currentNum].concat(remainingNumsPermuted[j]);
+            result.push(permutedArray);
+        }
     }
     return result;
-   }
+}
+
+console.log(biggerFunc('hey'))
 
 /* https://medium.com/weekly-webtips/step-by-step-guide-to-array-permutation-using-recursion-in-javascript-4e76188b88ff */
