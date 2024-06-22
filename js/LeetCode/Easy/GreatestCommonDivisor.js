e69371389e6d29c8fa1705958056addc8e6f5350
Brooke Kanonik <brookekanonik@gmail.com>
2024-06-22 13:12:54 -0400

    adding easy leetcode challenge

--- js/LeetCode/Easy/GreatestCommonDivisor.js
+++ js/LeetCode/Easy/GreatestCommonDivisor.js
@@ -46,3 +46,26 @@ var gcdOfStrings = function(str1, str2) {
     }
 };
 
+//Best practice:
+
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+    if (str1 + str2 != str2 + str1) return ''
+    let m = str1.length 
+    let n = str2.length 
+
+    let gcd = function (x,y){
+        if (!y) return x 
+        return gcd(y, x % y) 
+    }
+
+    let div = gcd(m,n)
+    return str1.slice(0, div)
+};
+
+//o(n) time complexity
+//ref: https://www.youtube.com/watch?v=DJHPBFmDtTw
