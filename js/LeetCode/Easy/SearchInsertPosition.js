eef98cf907feea8897cc8af6056df40392572151
Brooke Kanonik <brookekanonik@gmail.com>
2024-07-24 11:59:39 -0400

    another go at search insert

--- js/LeetCode/Easy/SearchInsertPosition.js
+++ js/LeetCode/Easy/SearchInsertPosition.js
@@ -128,3 +128,20 @@ var searchInsert = function(nums, target) {
     }
     return left;    
 };
+
+var searchInsert = function(nums, target) {
+    //let ans = 0
+    let left = 0
+    let right = nums.length - 1
+    while (left <= right){
+        let mid = Math.floor((left + right)/2)
+        if (nums[mid] === target){ //if the index has the target
+            return mid
+        }else if (nums[mid] < target){ //if target is bigger than mid val
+            left = mid + 1
+        }else {
+            right = mid - 1
+        }
+    }
+    return left
+};
