780b1268dce7feda6521a07701b09e90b9797cc9
Brooke Kanonik <brookekanonik@gmail.com>
2024-08-09 18:56:16 -0400

    adding 7kyu challenge

---
+++ js/7Kyu/RoundNextMultiple.js
@@ -0,0 +1,31 @@
+//Instructions:
+
+/*
+
+Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+
+Examples:
+
+input:    output:
+0    ->   0
+2    ->   5
+3    ->   5
+12   ->   15
+21   ->   25
+30   ->   30
+-2   ->   0
+-5   ->   -5
+etc.
+Input may be any positive or negative integer (including 0).
+
+You can assume that all inputs are valid integers.
+
+*/
+
+//Initial code (works and best practice):
+
+function roundToNext5(n){
+    let amount = n/5
+    return Math.ceil(amount) * 5
+}
+    
