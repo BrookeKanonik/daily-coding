54e67504cb7cdbe7a02b79f1d39c77501feed22f
Brooke Kanonik <brookekanonik@gmail.com>
2024-09-06 14:35:57 -0400

    adding new solution for med leetcode

--- js/LeetCode/Medium/BestTimeStock2.js
+++ js/LeetCode/Medium/BestTimeStock2.js
@@ -54,3 +54,22 @@ var maxProfit = function(prices) {
     }
     return profit
 };
+
+//New Ans:
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let lowest = prices[0]
+    let profit = 0
+
+    for (let i = 1; i < prices.length; i++){
+        if (prices[i] > lowest){
+            profit += prices[i] - lowest
+        }
+        lowest = prices[i]
+    }
+    return profit 
+};
