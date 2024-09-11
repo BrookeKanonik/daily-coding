d8cce924ea4a74586ccea3e365b5c5e8a791932a
Brooke Kanonik <brookekanonik@gmail.com>
2024-09-11 14:17:41 -0400

    adding easy leetcode challenge

---
+++ js/LeetCode/Easy/IsSubsequence.js
@@ -0,0 +1,58 @@
+// Instructions:
+
+/* Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+ 
+
+Constraints:
+
+0 <= s.length <= 100
+0 <= t.length <= 104
+s and t consist only of lowercase English letters.
+ 
+
+Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?
+
+*/
+
+//Initial code (works):
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let firstPointer = 0
+    for (let i=0; i<t.length; i++){
+        if (s[firstPointer] === t[i]){
+            firstPointer ++
+        }
+    }
+    return firstPointer === s.length
+};
+
+//Maybe want to add a break point so if it is 30000 characters and we find it in the first 5, we do not go on forever
+
+var isSubsequence = function(s, t) {
+    let firstPointer = 0
+    for (let i=0; i<t.length; i++){
+        if (firstPointer === s.length) break
+        if (s[firstPointer] === t[i]){
+            firstPointer ++
+        }
+    }
+    return firstPointer === s.length
+};
