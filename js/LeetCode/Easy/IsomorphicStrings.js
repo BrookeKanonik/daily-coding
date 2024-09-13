42ba1ad2008f0610655e35d3ee7cbe772646eef4
Brooke Kanonik <brookekanonik@gmail.com>
2024-09-13 11:14:25 -0400

    adding easy leetcode challenge

---
+++ js/LeetCode/Easy/IsomorphicStrings.js
@@ -0,0 +1,79 @@
+/* Instructions:
+
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+ 
+
+Example 1:
+
+Input: s = "egg", t = "add"
+
+Output: true
+
+Explanation:
+
+The strings s and t can be made identical by:
+
+Mapping 'e' to 'a'.
+Mapping 'g' to 'd'.
+Example 2:
+
+Input: s = "foo", t = "bar"
+
+Output: false
+
+Explanation:
+
+The strings s and t can not be made identical as 'o' needs to be mapped to both 'a' and 'r'.
+
+Example 3:
+
+Input: s = "paper", t = "title"
+
+Output: true
+
+ 
+
+Constraints:
+
+1 <= s.length <= 5 * 104
+t.length == s.length
+s and t consist of any valid ascii character.
+
+*/
+
+//Initial code (works):
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    let resource = {}
+
+    if (s.length !== t.length){
+        return false
+    }else{
+        for (let i=0; i <s.length; i++){
+            if (resource[s[i]] === undefined){
+                if (Object.values(resource).includes(t[i])){
+                    return false
+                }else{
+                    resource[s[i]] = t[i]
+                }
+            }else{
+                if (resource[s[i]] !== t[i]){
+                return false
+                }
+            }
+        }
+    }
+    return true
+};
+
+//can remove the if else statement for length
