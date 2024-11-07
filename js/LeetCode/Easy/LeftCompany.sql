4dea342cfad344c521686f6d573cdc45f3d16728
Brooke Kanonik <brookekanonik@gmail.com>
2024-11-07 14:31:04 -0500

    adding sql challenge involving not null and nested selects

--- js/LeetCode/Easy/LeftCompany.sql
+++ js/LeetCode/Easy/LeftCompany.sql
@@ -56,3 +56,16 @@ SELECT a.employee_id from Employees a
 LEFT JOIN Employees b ON a.manager_id = b.employee_id
 WHERE a.manager_id IS NOT NULL AND b.employee_id IS NULL AND a.salary < 30000
 ORDER BY a.employee_id 
+
+/*Other method: */
+
+SELECT 
+    employee_id
+FROM employees
+WHERE salary < 30000 
+    AND manager_id NOT IN (
+        SELECT 
+            employee_id 
+        FROM employees
+    )
+ORDER BY employee_id ASC;
