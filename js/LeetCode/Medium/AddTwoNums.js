82a420d68a8d97d2bbaad3b0ef3813cdff2d3def
Brooke Kanonik <brookekanonik@gmail.com>
2025-09-16 14:31:33 -0400

    adding additional work. need to convert final number to linked list

--- js/LeetCode/Medium/AddTwoNums.js
+++ js/LeetCode/Medium/AddTwoNums.js
@@ -50,3 +50,59 @@ var addTwoNumbers = function(l1, l2) {
 
     //rewrite with head, next and tail
 };
+
+//Recent work:
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+let newList = new LinkedList()
+let firstNum = ''
+let secondNum = ''
+var addTwoNumbers = function(l1, l2) {
+    //go through whcihever is longer
+    // let longest = 0;
+    // if (l1.size > l2.size){
+    //     longest = l1
+    // }else{
+    //     longest = l2 //can write as terinary operator 
+    // }
+    // console.log(longest.size)
+    while (l1 || l2){
+        console.log(l1.val)
+        l1.val == undefined ? firstNum += 0 : firstNum += l1.val
+        l2.val == undefined ? secondNum += 0 : secondNum += l2.val
+
+        l1 = l1.next;
+        l2 = l2.next;
+    } //go through each and add
+    firstNum = parseInt(firstNum)
+    secondNum = parseInt(secondNum)
+    newTotal = firstNum + secondNum
+
+    console.log(newTotal)
+    
+    //make this number a linked list NEED TO WORK ON
+    newList.head = newTotal[newTotal.length-1] //first part
+    for (let i=newTotal.length -2; i>=0; i--){
+        newList.append = newTotal[i]
+        newList = newList.next;
+    }
+
+    return newList
+
+    //rewrite with head, next and tail
+
+    
+    
+};
